2a38309f023493ae9d627d418180984a4bdc86f0
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-07 14:21:46 +0300

    add logic to dashboardfor contractors.

--- app/src/main/java/com/christine/dreamhouseapp/DashBoardContractors.java
+++ app/src/main/java/com/christine/dreamhouseapp/DashBoardContractors.java
@@ -1,14 +1,51 @@
 package com.christine.dreamhouseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DashBoardContractors extends AppCompatActivity {
+    @BindView(R.id.contractorsTextView) TextView mContractorTextView;
+    @BindView(R.id.contractorsListView) ListView mContractorsListView;
+    private String[] contractors = new String[] {"Henry Matthews","Timothy Owindi","MiCkey Jones","Victoria Rumba","Justin Matthews","Bold Binther"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board_contractors);
+
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,contractors);
+        mContractorsListView.setAdapter(adapter);
+
+        mContractorsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String contractor = ((TextView)view).getText().toString();
+                FragmentManager fm = getSupportFragmentManager();
+                ProfileActivity pActivity = new ProfileActivity();
+                pActivity.show(fm,"simple Fragments");
+//                Toast.makeText(DashBoardContractors.this, contractor, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mContractorTextView.setText("Here are all the contractors: ");
+
+
+
+
+
     }
 }
