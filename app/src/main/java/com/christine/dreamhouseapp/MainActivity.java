c5cff04fb2de68284047ae053690d31218d707f9
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-07 04:28:25 +0300

    add logic to the login layout activity.

--- app/src/main/java/com/christine/dreamhouseapp/MainActivity.java
+++ app/src/main/java/com/christine/dreamhouseapp/MainActivity.java
@@ -2,13 +2,59 @@ package com.christine.dreamhouseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    @Override
+public class MainActivity extends AppCompatActivity{
+    @BindView(R.id.loginButton)Button mLoginButton;
+    @BindView(R.id.signUpButton)Button mSignUpButton;
+    @BindView(R.id.emailEditText)EditText mEmail;
+    @BindView(R.id.passwordEditText)EditText mPassword;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,signup.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
+
+            mLoginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String email = mEmail.getText().toString();
+                    showToast(email);
+                    Intent i = new Intent(MainActivity.this,DashBoardContractors.class);
+                    startActivity(i);
+                }
+
+
+        });
+
+
+
+
+    }
+    private void showToast(String text){
+        Toast.makeText(MainActivity.this,text,Toast.LENGTH_LONG).show();
+
+
     }
 }
