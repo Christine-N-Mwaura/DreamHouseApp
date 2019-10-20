c4ea5c689b04524bb63ae71d3c9aa4f6198d1767
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-20 20:49:26 +0300

    add profile activity.

--- app/src/main/java/com/christine/dreamhouseapp/ProfileActivity.java
+++ app/src/main/java/com/christine/dreamhouseapp/ProfileActivity.java
@@ -1,9 +1,7 @@
 package com.christine.dreamhouseapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +13,7 @@ public class ProfileActivity extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view  = inflater.inflate(R.layout.activity_profile,container,false);
-        Button mProfile = (Button) view.findViewById(R.id.goBackButton);
+        Button mProfile = (Button) view.findViewById(R.id.addImageButton);
 
         mProfile.setOnClickListener(new View.OnClickListener() {
             @Override
