9c9f4c836c8b76380c93092ce1da0a073ba7b47d
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-21 20:22:42 +0300

    add the required paths to display images.

--- app/src/main/java/com/christine/dreamhouseapp/network/GetMovieDataService.java
+++ app/src/main/java/com/christine/dreamhouseapp/network/GetMovieDataService.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface GetMovieService {
+public interface GetMovieDataService {
     @GET("movie/popular")
     Call<MoviePageResult>getPopularMovies(@Query("page") int page, @Query("api_key")String userkey);
 
