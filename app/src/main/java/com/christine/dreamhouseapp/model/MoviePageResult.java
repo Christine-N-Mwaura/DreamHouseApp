8b13bdcf60de8c801df2d91c463344bfe4c8badb
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-21 07:05:14 +0300

    add a package named model with the generated java classes

--- app/src/main/java/com/christine/dreamhouseapp/model/MoviePageResult.java
+++ app/src/main/java/com/christine/dreamhouseapp/model/MoviePageResult.java
@@ -1,4 +1,64 @@
 package com.christine.dreamhouseapp.model;
 
-public class MoviePage {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MoviePageResult implements Serializable {
+
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("results")
+    @Expose
+    private ArrayList<Movie> movieResult = null;
+    private final static long serialVersionUID = 9036741358049641864L;
+
+    public MoviePageResult(int page, int totalResults, int totalPages, ArrayList<Movie> movieResult) {
+        this.page = page;
+        this.totalResults = totalResults;
+        this.totalPages = totalPages;
+        this.movieResult = movieResult;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public ArrayList<Movie> getMovieResults() {
+        return movieResult;
+    }
+
+    public void setMovieResults(ArrayList<Movie> movieResult) {
+        this.movieResult = movieResult;
+    }
+
 }
--- app/src/main/java/com/christine/dreamhouseapp/model/MovieTrailerResult.java
+++ app/src/main/java/com/christine/dreamhouseapp/model/MovieTrailerResult.java
@@ -1,4 +1,30 @@
 package com.christine.dreamhouseapp.model;
 
-public class MovieTrailerResult {
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MovieTrailerResult implements Serializable {
+
+    @SerializedName("id")
+    private int id;
+    @SerializedName("results")
+    private ArrayList<MovieTrailer> trailerResult;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<MovieTrailer> getTrailerResult() {
+        return trailerResult;
+    }
+
+    public void setTrailerResult(ArrayList<MovieTrailer> trailerResult) {
+        this.trailerResult = trailerResult;
+    }
 }
