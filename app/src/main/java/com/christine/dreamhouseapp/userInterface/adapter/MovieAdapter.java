52356b9ccaea97f200156fb41ef278b15121284d
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-21 08:45:34 +0300

    edit movie adapter file

--- app/src/main/java/com/christine/dreamhouseapp/userInterface/adapter/MovieAdapter.java
+++ app/src/main/java/com/christine/dreamhouseapp/userInterface/adapter/MovieAdapter.java
@@ -1,4 +1,44 @@
 package com.christine.dreamhouseapp.userInterface.adapter;
 
-public class MovieAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.christine.dreamhouseapp.R;
+import com.christine.dreamhouseapp.model.Movie;
+import com.christine.dreamhouseapp.userInterface.utils.MovieClickListener;
+import com.christine.dreamhouseapp.userInterface.viewHolder.MovieViewHolder;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+
+    private final MovieClickListener movieClickListener;
+    private final List<Movie> movieList;
+
+    public MovieAdapter(List<Movie> movieList, MovieClickListener movieClickListener){
+
+        this.movieList = movieList;
+        this.movieClickListener = movieClickListener;
+
+    }
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_card_view,parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position){
+        Movie movie = this.movieList.get(position);
+        holder.bind(movie, movieClickListener);
+    }
+
+    @Override
+    public int getItemCount(){return this.movieList.size();}
+
+    @Override
+    public void onViewRecycled(MovieViewHolder holder){super.onViewRecycled(holder);}
 }
