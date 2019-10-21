8c5d9e9fe98226f9ab5bb85f7c098016f578107b
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-10-21 08:47:55 +0300

    add logic for when a movie poster is clicked.

--- app/src/main/java/com/christine/dreamhouseapp/userInterface/utils/MovieClickListener.java
+++ app/src/main/java/com/christine/dreamhouseapp/userInterface/utils/MovieClickListener.java
@@ -1,4 +1,8 @@
 package com.christine.dreamhouseapp.userInterface.utils;
 
+import com.christine.dreamhouseapp.model.Movie;
+
 public interface MovieClickListener {
+
+    void onMovieClick(Movie movie);
 }
